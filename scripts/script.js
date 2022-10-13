22e164fc92214ceddf4fa930c9d96e52ad8adda3
Giulliano Guimarães <llthguimaraes@gmail.com>
2022-10-13 18:00:23 -0300

    Input name validate 3 - JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const cardName = document.querySelector(".j_card_name");
 const cardTemplateName = document.querySelector(".j_card_template_name");
+const cardFront = document.querySelector(".j_card_front");
 
 const errorMessage = (text) => {
     let spanMessage = document.createElement("span");
@@ -15,12 +16,14 @@ cardName.addEventListener("keyup", (event) => {
     let formKeyCode = event.code;
 
     if (cardName.value.length === 0 && !cardName.parentElement.lastElementChild.classList.contains("message")) {
+        cardFront.classList.add("card_alert");
         cardName.classList.add("error");
         cardName.parentElement.append(errorMessage("Can't be blank"));
     }
 
     if (formKeyCode.substring(0, 3) === "Key" || formKeyCode === "Semicolon" || formKeyCode === "Space") {
 
+        cardFront.classList.remove("card_alert");
         cardName.classList.remove("error");
 
         if (cardName.parentElement.lastElementChild.classList.contains("message")) {
@@ -37,15 +40,32 @@ cardName.addEventListener("keyup", (event) => {
         }
 
     } else {
-        
-        if (formKeyCode === "Backspace") {
+
+        if (formKeyCode === "Backspace" || formKeyCode === "Delete") {
             if (cardName.value.length !== 0) {
                 cardTemplateName.innerHTML = cardName.value;
             } else {
                 cardTemplateName.innerHTML = "---";
             }
+        } else {
+
+            cardFront.classList.add("card_alert");
+            if (!cardName.parentElement.lastElementChild.classList.contains("message")) {
+                cardName.classList.add("error");
+                cardName.parentElement.append(errorMessage("Invalid format, letters only"));
+            }
+            
+            if (cardName.value.length === 0) {
+                cardFront.classList.remove("card_alert");
+                cardTemplateName.innerHTML = "---";
+            } else if (cardName.value.length === 1) {
+                cardTemplateName.innerHTML = "";
+                cardTemplateName.textContent += formChar;
+            } else if (cardName.value.length > 1) {
+                cardTemplateName.textContent += formChar;
+            }
         }
     }
 
-    // console.log(cardName.value[cardName.value.length - 1]);
+    console.log(formKeyCode);
 })
