42ea2cba1b34ad163037e05be86441f2e7499cdc
Giulliano Guimarães <llthguimaraes@gmail.com>
2022-10-13 17:03:56 -0300

    Input name validate 2 - JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,44 @@
 const cardName = document.querySelector(".j_card_name");
 const cardTemplateName = document.querySelector(".j_card_template_name");
 
-const message = () => {
-    
+const errorMessage = (text) => {
+    let spanMessage = document.createElement("span");
+    spanMessage.className = "message";
+    spanMessage.innerText = text;
+    return spanMessage;
 }
 
 cardName.addEventListener("keyup", (event) => {
     event.preventDefault();
 
-    if (cardName.value.length === 0) {
+    let formChar = event.key;
+    let formKeyCode = event.code;
+
+    if (cardName.value.length === 0 && !cardName.parentElement.lastElementChild.classList.contains("message")) {
         cardName.classList.add("error");
+        cardName.parentElement.append(errorMessage("Can't be blank"));
     }
 
-    if (event.code.substring(0, 3) === "Key" || event.code === "Semicolon" || event.code === "Space") {
+    if (formKeyCode.substring(0, 3) === "Key" || formKeyCode === "Semicolon" || formKeyCode === "Space") {
 
         cardName.classList.remove("error");
+
+        if (cardName.parentElement.lastElementChild.classList.contains("message")) {
+            cardName.parentElement.lastElementChild.remove();
+        }
 
         if (cardName.value.length === 0) {
             cardTemplateName.innerHTML = "---";
         } else if (cardName.value.length === 1) {
             cardTemplateName.innerHTML = "";
-            cardTemplateName.textContent += event.key;
+            cardTemplateName.textContent += formChar;
         } else if (cardName.value.length > 1) {
-            cardTemplateName.textContent += event.key;
+            cardTemplateName.textContent += formChar;
         }
 
     } else {
         
-        if (event.code === "Backspace") {
+        if (formKeyCode === "Backspace") {
             if (cardName.value.length !== 0) {
                 cardTemplateName.innerHTML = cardName.value;
             } else {
