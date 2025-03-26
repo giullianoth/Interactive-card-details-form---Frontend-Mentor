36ad94d9aaafdfc9a1031ea20116ba22c7a617d6
Giulliano <tharsoweb@gmail.com>
2025-03-26 12:20:56 -0300

    validate name

--- assets/scripts/validate-name.js
+++ assets/scripts/validate-name.js
@@ -15,8 +15,6 @@ export default function ValidateName() {
             errorMessage("Can't be blank", "name", cardNameLabel)
         }
 
-        console.log(cardNameLength);
-
         if (cardNameLength <= cardNameMaxLength) {
             cardNameTemplate.innerText = cardNameLength
                 ? cardNameValue
